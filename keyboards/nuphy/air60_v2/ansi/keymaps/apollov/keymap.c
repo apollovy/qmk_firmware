ce9274cd963742fa5b453b793dcb52878e305f78
Yuri Apollov <apollovy@icloud.com>
2025-02-04 19:41:29 +0300

    Update my keymap.

--- keyboards/nuphy/air60_v2/ansi/keymaps/apollov/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/apollov/keymap.c
@@ -26,14 +26,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, 	    KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,
 	KC_CAPS,	    KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	            KC_ENT,
 	KC_LSFT,	    KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_DEL,
-	KC_LCTL,	    KC_LALT,	KC_LGUI,									LT(1,KC_SPC), 							KC_RGUI,	TT(1),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
+    KC_LCTL,	    KC_LALT,	KC_LGUI,									LT(1,KC_SPC), 							KC_RGUI,	MO(1),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer 1 Mac fn
 [1] = LAYOUT(
 	KC_GRV, 	KC_BRID,  	KC_BRIU,  	MAC_TASK, 	MAC_SEARCH, MAC_VOICE,  MAC_DND,  	KC_MPRV,  	KC_MPLY,  	KC_MNXT, 	KC_MUTE, 	KC_VOLD, 	KC_VOLU, 	_______,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,
 	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	            RGB_RMOD,
-	_______,    _______,   	_______,   	_______,  	_______,   	_______,   	_______,   	TT(6),   	RGB_SPD,	RGB_SPI,  	_______,	TT(2),  	RGB_VAI,    _______,
+	MO(2),      _______,   	_______,   	_______,  	_______,   	_______,   	_______,   	MO(6),   	RGB_SPD,	RGB_SPI,  	_______,	MO(2),  	RGB_VAI,    _______,
 	_______,	_______,	_______,										BAT_SHOW, 							_______,	_______,   	RGB_MOD,	RGB_VAD,    RGB_HUI),
 
 // layer 2 Mac Fn+shift
@@ -50,14 +50,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_TAB, 	    KC_Q,   	KC_W,   	KC_E,  		KC_R,   	KC_T,   	KC_Y,   	KC_U,   	KC_I,   	KC_O,  		KC_P,   	KC_LBRC,	KC_RBRC, 	KC_BSLS,
 	KC_CAPS,	    KC_A,   	KC_S,   	KC_D,  		KC_F,   	KC_G,   	KC_H,   	KC_J,   	KC_K,   	KC_L,  		KC_SCLN,	KC_QUOT, 	            KC_ENT,
 	KC_LSFT,	    KC_Z,   	KC_X,   	KC_C,  		KC_V,   	KC_B,   	KC_N,   	KC_M,   	KC_COMM,	KC_DOT,		KC_SLSH,	KC_RSFT,	KC_UP,		KC_DEL,
-	KC_LCTL,	    KC_LGUI,	KC_LALT,									LT(4,KC_SPC), 							KC_RALT,	TT(4),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
+	KC_LCTL,	    KC_LGUI,	KC_LALT,									LT(4,KC_SPC), 							KC_RALT,	MO(4),   	KC_LEFT,	KC_DOWN,    KC_RGHT),
 
 // layer 4 win fn
 [4] = LAYOUT(
 	KC_GRV, 	KC_F1,  	KC_F2,  	KC_F3, 		KC_F4,  	KC_F5,  	KC_F6,  	KC_F7,  	KC_F8,  	KC_F9, 		KC_F10, 	KC_F11, 	KC_F12, 	_______,
 	_______, 	LNK_BLE1,  	LNK_BLE2,  	LNK_BLE3,  	LNK_RF,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	DEV_RESET,	SLEEP_MODE, BAT_SHOW,
 	_______, 	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	_______,   	_______,   	_______,  	_______,   	_______,	            _______,
-	_______,	_______,   	_______,   	_______,  	_______,   	_______,   	_______,   	TT(6),   	RGB_SPD,	RGB_SPI,  	_______,	TT(5),  	RGB_VAI,    _______,
+	MO(5),	    _______,   	_______,   	_______,  	_______,   	_______,   	_______,   	MO(6),   	RGB_SPD,	RGB_SPI,  	_______,	MO(5),  	RGB_VAI,    _______,
 	_______,	_______,	_______,										BAT_SHOW, 							_______,	_______,   	RGB_MOD,	RGB_VAD,    RGB_HUI),
 
 // layer 5 win fn+shift
